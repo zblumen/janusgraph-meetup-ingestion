9fe677ecd82d17bf822ee145828c473ad73d57bb
Zblumen <zblumenf@gmail.com>
2020-02-17 03:41:12 +0000

    WIP: Removing old classes and cleaning up methods

--- graph_data_ingest/graph_ingest.py
+++ graph_data_ingest/graph_ingest.py
@@ -14,45 +14,8 @@ GraphElementStagingData = Dict[str, pd.DataFrame]
 VertexStagingDataMap = Dict[str, GraphElementStagingData]
 
 
-def instantiate_staging_data_frame(self, schema: sc.GraphStagingSchema) -> pd.DataFrame:
+def instantiate_staging_data_frame(schema: sc.GraphStagingSchema) -> pd.DataFrame:
     return pd.DataFrame(columns=list(schema.properties.keys())).astype(schema)
-
-
-class GraphIngestData:
-    vertexStagingSchemaMap: Dict[str, sc.GraphStagingSchema]
-    vertexStagingDataMap: Dict[str, pd.DataFrame]
-
-    edgeStagingSchema: sc.GraphStagingSchema
-    edgeStagingData: pd.DataFrame
-
-    def __init__(self,
-                 vertex_staging_schema_map: Dict[str, sc.GraphStagingSchema],
-                 edge_staging_schema: sc.GraphStagingSchema
-                 ):
-
-        # create vertex schemas
-        self.vertexStagingSchemaMap = vertex_staging_schema_map
-        for key, value in vertex_staging_schema_map.items():
-            self.vertexStagingDataMap[key] = instantiate_staging_data_frame(value)
-
-        # create edge schema
-        self.edgeStagingSchema = edge_staging_schema
-        self.edgeStagingData = instantiate_staging_data_frame(edge_staging_schema)
-
-    def staged_vertex_exists(self, tracking_id: str, label: str):
-        return tracking_id in self.vertexStagingDataMap[label].index
-
-    def get_staged_vertex(self, tracking_id: str, label: str):
-        return self.vertexStagingDataMap[label].loc[tracking_id, :].se
-
-    def stage_vertex(self, tracking_id: str, label: str, properties: Dict, check_schema=True):
-
-        if label not in self.vertexStagingSchemaMap:
-            raise RuntimeError("Cannot find the Vertex Label " + label + " in vertex schema staging schema map.")
-        if check_schema:
-            self.vertexStagingSchemaMap[label].check_property_fields(label, inc=False)
-
-        self.vertexStagingDataMap[label].loc[tracking_id] = properties
 
 
 class GraphIngestTracker:
